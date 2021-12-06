8cb71bb4ac7db1138b85ae082dfc80a51282eb11
Matthew Rankin <matthew@cumulusware.com>
2021-12-06 15:01:15 -0600

    Add various ratios

--- ratios.go
+++ ratios.go
@@ -5,6 +5,79 @@
 
 package fin
 
+///////////////////////////////////////////////////////////////////////////////
+//
+// Profitability Ratios
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// ReturnOnEquity tells what percentage of profit is made for every dollar of
+// equity invested in the company.
+//
+// Source: https://financialintelligencebook.com
+func ReturnOnEquity(netProfit, equity float64) float64 {
+	return netProfit / equity
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Leverage Ratios
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// DebtToAssets measures the ratio of all debt (short-term debt and long-term
+// debt, but not other liabilities) to all assets.
+func DebtToAssets(debt, assets float64) float64 {
+	return debt / assets
+}
+
+// DebtToEquity measures the ratio of all debt (short-term debt and long-term
+// debt, but not other liabilities) to shareholders' equity.
+func DebtToEquity(debt, equity float64) float64 {
+	return debt / equity
+}
+
+// LiabilitiesToAssets measures the ratio of total liabilities to total assets.
+func LiabilitiesToAssets(liabilities, assets float64) float64 {
+	return liabilities / assets
+}
+
+// LiabilitiesToEquity measures the ratio of total liabilities to total
+// shareholders' equity.
+func LiabilitiesToEquity(liabilities, equity float64) float64 {
+	return liabilities / equity
+}
+
+// EquityMultiplier ratio is the factor by which the return on assets is
+// multiplied to determine the return on equity. It is defined as the ratio of
+// total assets to common equity. [Source: Financial Management: Theory &
+// Practice, 16th ed.]
+func EquityMultiplier(assets, equity float64) float64 {
+	return assets / equity
+}
+
+// TimesInterestEarned ratio, also called the interest coverage ratio, is
+// determined by dividing earnings before interest and taxes (EBIT) by the
+// interest expense. [Source: Financial Management: Theory & Practice, 16th
+// ed.]
+func TimesInterestEarned(ebit, interest float64) float64 {
+	return ebit / interest
+}
+
+// InterestCoverage calculates the ratio of many times it can pay its annual
+// interest charges.
+//
+// Source: https://financialintelligencebook.com
+func InterestCoverage(operatingProfit, interest float64) float64 {
+	return operatingProfit / interest
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Liquidity Ratios
+//
+///////////////////////////////////////////////////////////////////////////////
+
 // CurrentRatio measures a copmany's current assets against its current
 // liabilities.
 //
@@ -13,10 +86,86 @@ func CurrentRatio(currentAssets, currentLiabilities float64) float64 {
 	return currentAssets / currentLiabilities
 }
 
-// QuickRatio also known as the acid test, assesses how quickly a company could
-// pay off their short-term debt without waiting to sell inventory.
+// QuickRatio, also known as the acid test, assesses how quickly a company
+// could pay off their short-term debt without waiting to sell inventory.
 //
 // Source: https://financialintelligencebook.com
 func QuickRatio(currentAssets, currentLiabilities, inventory float64) float64 {
 	return (currentAssets - inventory) / currentLiabilities
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Efficiency Ratios
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// DaysInInventory measures the number of days inventory stays in the system.
+//
+// Source: https://financialintelligencebook.com
+func DaysInInventory(inventory, cogs float64, days int) float64 {
+	return inventory / (cogs / float64(days))
+}
+
+// DaysInInventory360 measures the number of days inventory stays in the system
+// assuming 360 days for the year.
+//
+// Source: https://financialintelligencebook.com
+func DaysInInventory360(inventory, cogs float64) float64 {
+	return DaysInInventory(inventory, cogs, 360)
+}
+
+// InventoryTurns measures how many times inventory turns over in a year.
+//
+// Source: https://financialintelligencebook.com
+func InventoryTurns(inventory, cogs float64) float64 {
+	return cogs / inventory
+}
+
+// DaysSalesOutstanding measures the average time it takes to collect the
+// cash from sales.
+//
+// Source: https://financialintelligencebook.com
+func DaysSalesOutstanding(receivables, revenues float64, days int) float64 {
+	return receivables / (revenues / float64(days))
+}
+
+// DaysSalesOutstanding360 measures the average time it takes to collect the
+// cash from sales assuming 360 days for the year.
+//
+// Source: https://financialintelligencebook.com
+func DaysSalesOutstanding360(receivables, revenues float64) float64 {
+	return DaysSalesOutstanding(receivables, revenues, 360)
+}
+
+// DaysPayableOutstanding shows the average number of days it take a company to
+// pay its own outstanding invoices.
+//
+// Source: https://financialintelligencebook.com
+func DaysPayableOutstanding(payables, cogs float64, days int) float64 {
+	return payables / (cogs / float64(days))
+}
+
+// DaysPayableOutstanding360 shows the average number of days it take a company to
+// pay its own outstanding invoices assuming 360 days for the year.
+//
+// Source: https://financialintelligencebook.com
+func DaysPayableOutstanding360(payables, cogs float64) float64 {
+	return DaysPayableOutstanding(payables, cogs, 360)
+}
+
+// PPETurnover calculates the amount of revenue generated per dollar invested
+// in Property, Plant, and Equipment (PPE).
+//
+// Source: https://financialintelligencebook.com
+func PPETurnover(revenue, ppe float64) float64 {
+	return revenue / ppe
+}
+
+// TotalAssetTurnover calculates the amount of revenue generated per dollar
+// invested in all assets.
+//
+// Source: https://financialintelligencebook.com
+func TotalAssetTurnover(revenue, assets float64) float64 {
+	return revenue / assets
 }
---
+++ ratios_test.go
@@ -0,0 +1,382 @@
+// Copyright (c) 2019-2022 The goinvest/fin developers. All rights reserved.
+// Project site: https://github.com/goinvest/fin
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE file for the project.
+
+package fin
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func TestReturnOnEquity(t *testing.T) {
+	testCases := []struct {
+		netProfit float64
+		equity    float64
+		want      float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("return_on_equity_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := ReturnOnEquity(test.netProfit, test.equity)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDebtToAssets(t *testing.T) {
+	testCases := []struct {
+		debt   float64
+		assets float64
+		want   float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("debt_to_assets_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DebtToAssets(test.debt, test.assets)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDebtToEquity(t *testing.T) {
+	testCases := []struct {
+		debt   float64
+		equity float64
+		want   float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("debt_to_equity_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DebtToEquity(test.debt, test.equity)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestLiabilitiesToAssets(t *testing.T) {
+	testCases := []struct {
+		liabilities float64
+		assets      float64
+		want        float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("liabilities_to_assets_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := LiabilitiesToAssets(test.liabilities, test.assets)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestLiabilitiesToEquity(t *testing.T) {
+	testCases := []struct {
+		liabilities float64
+		equity      float64
+		want        float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("liabilities_to_equity_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := LiabilitiesToEquity(test.liabilities, test.equity)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestEquityMultiplier(t *testing.T) {
+	testCases := []struct {
+		assets float64
+		equity float64
+		want   float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("equity_multiplier_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := EquityMultiplier(test.assets, test.equity)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestTimesInterestEarned(t *testing.T) {
+	testCases := []struct {
+		ebit     float64
+		interest float64
+		want     float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("time_interest_earned_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := TimesInterestEarned(test.ebit, test.interest)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestInterestCoverage(t *testing.T) {
+	testCases := []struct {
+		operatingProfit float64
+		interest        float64
+		want            float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("interest_coverage_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := InterestCoverage(test.operatingProfit, test.interest)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestCurrentRatio(t *testing.T) {
+	testCases := []struct {
+		currentAssets      float64
+		currentLiabilities float64
+		want               float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("current_ratio_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := CurrentRatio(test.currentAssets, test.currentLiabilities)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestQuickRatio(t *testing.T) {
+	testCases := []struct {
+		currentAssets      float64
+		currentLiabilities float64
+		inventory          float64
+		want               float64
+	}{
+		{10.0, 2.0, 4.0, 3.0},
+		{250.0, 100.0, 50.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("quick_ratio_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := QuickRatio(test.currentAssets, test.currentLiabilities, test.inventory)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDaysInInventory(t *testing.T) {
+	testCases := []struct {
+		inventory float64
+		cogs      float64
+		days      int
+		want      float64
+	}{
+		{64.0, 720.0, 360, 32.0},
+		{125.0, 250.0, 360, 180.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("dii_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DaysInInventory(test.inventory, test.cogs, test.days)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDaysInInventory360(t *testing.T) {
+	testCases := []struct {
+		inventory float64
+		cogs      float64
+		want      float64
+	}{
+		{64.0, 720.0, 32.0},
+		{125.0, 250.0, 180.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("dii360_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DaysInInventory360(test.inventory, test.cogs)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestInventoryTurns(t *testing.T) {
+	testCases := []struct {
+		inventory float64
+		cogs      float64
+		want      float64
+	}{
+		{64.0, 720.0, 11.25},
+		{125.0, 250.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("inventory_turns_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := InventoryTurns(test.inventory, test.cogs)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDaysSalesOutstanding(t *testing.T) {
+	testCases := []struct {
+		receivables float64
+		revenues    float64
+		days        int
+		want        float64
+	}{
+		{10.0, 200.0, 360, 18.0},
+		{250.0, 500.0, 360, 180.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("days_sales_outstanding_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DaysSalesOutstanding(test.receivables, test.revenues, test.days)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDaysSalesOutstanding360(t *testing.T) {
+	testCases := []struct {
+		receivables float64
+		revenues    float64
+		want        float64
+	}{
+		{10.0, 200.0, 18.0},
+		{250.0, 500.0, 180.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("days_sales_outstanding_360_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DaysSalesOutstanding360(test.receivables, test.revenues)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDaysPayableOutstanding(t *testing.T) {
+	testCases := []struct {
+		payables float64
+		cogs     float64
+		days     int
+		want     float64
+	}{
+		{10.0, 20.0, 360, 180.0},
+		{250.0, 1000.0, 360, 90.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("days_payable_outstanding_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DaysPayableOutstanding(test.payables, test.cogs, test.days)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestDaysPayableOutstanding360(t *testing.T) {
+	testCases := []struct {
+		payables float64
+		cogs     float64
+		want     float64
+	}{
+		{10.0, 20.0, 180.0},
+		{250.0, 1000.0, 90.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("days_payable_outstanding_360_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := DaysPayableOutstanding360(test.payables, test.cogs)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestPPETurnover(t *testing.T) {
+	testCases := []struct {
+		revenue float64
+		ppe     float64
+		want    float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("ppe_turnover_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := PPETurnover(test.revenue, test.ppe)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func TestTotalAssetTurnover(t *testing.T) {
+	testCases := []struct {
+		revenue float64
+		assets  float64
+		want    float64
+	}{
+		{10.0, 2.0, 5.0},
+		{250.0, 125.0, 2.0},
+	}
+	for i, test := range testCases {
+		name := fmt.Sprintf("total_asset_turnover_%d", i)
+		t.Run(name, func(t *testing.T) {
+			got := TotalAssetTurnover(test.revenue, test.assets)
+			assertFloat64(t, name, got, test.want, 0.0001)
+		})
+	}
+}
+
+func assertInt(t *testing.T, label string, got, want int) {
+	if got != want {
+		t.Errorf("\t got = %d %s\n\t\twant = %d", got, label, want)
+	}
+}
+
+func assertFloat64(t *testing.T, label string, got, want, tolerance float64) {
+	if diff := math.Abs(want - got); diff >= tolerance {
+		t.Errorf("\t got = %f %s\n\t\t\twant = %f", got, label, want)
+	}
+}
+
+func assertBool(t *testing.T, label string, got, want bool) {
+	if got != want {
+		t.Errorf("\t got = %t %s\n\t\t\twant = %t", got, label, want)
+	}
+}
+
+func assertString(t *testing.T, label string, got, want string) {
+	if got != want {
+		t.Errorf("\t got = %s %s\n\t\t\twant = %s", got, label, want)
+	}
+}
